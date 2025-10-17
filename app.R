b45a9d4c66d687939b94e2a771bd58caa0e64bdd
SIGEHgo <sigeh@hidalgo.gob.mx>
2025-10-17 11:30:39 -0600

    Se añadio a app las variables

--- app.R
+++ app.R
@@ -3,6 +3,30 @@ library(DBI)
 library(RPostgres)
 library(DT)
 
+
+# Obtener las variables de entorno para la conexión
+db_host <- Sys.getenv("db_host")
+db_user <- Sys.getenv("db_user")
+db_pass <- Sys.getenv("db_pass")
+db_port <- as.numeric(Sys.getenv("db_port")) # Asegúrate de convertir el puerto a numérico
+db_name <- Sys.getenv("db_name")
+
+##Codigo para conectarnos a sql usando dplyr
+library(DBI)
+library(RPostgres)
+buig <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  dbname=db_name,
+  host = db_host,
+  port = db_port,
+  user = db_user,
+  password = db_pass
+)
+#DBI::dbExecute(buig, "SET search_path TO emergencia;")
+
+
+Lista_BUIG=DBI::dbListTables(buig) |> as.list()
+
 ui <- fluidPage(
   fluidRow(
     column(
